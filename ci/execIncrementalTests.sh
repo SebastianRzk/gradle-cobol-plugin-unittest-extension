5c3590f928c9ac2a315cc1fb3f8922d129eace77
RosesTheN00b <sebastian.r.ruziczka@gmail.com>
2018-05-14 10:57:34 +0200

    added bas and switch offline task properly

--- ci/execIncrementalTests.sh
+++ ci/execIncrementalTests.sh
@@ -26,10 +26,7 @@ echo "<<<<<<<"
 
 
 echo ">>>>>>> prepare test-repository"
-rm -v endToEndTest/gradle-cobol-plugin-example/settings.gradle
-cp endToEndTest/gradle-cobol-plugin-example/ci/local_repo_test_settings.gradle endToEndTest/gradle-cobol-plugin-example/settings.gradle
-rm -v endToEndTest/gradle-cobol-plugin-example/build.gradle
-cp ci/build.gradle.replacement endToEndTest/gradle-cobol-plugin-example/build.gradle
+gradle generateVersions switchOffline
 echo "<<<<<<<"
 
 echo ">>>>>>> exec test"
