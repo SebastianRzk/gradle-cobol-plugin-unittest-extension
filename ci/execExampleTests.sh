5cd491b81eb771e768e87e363405c70bde05083e
RosesTheN00b <git@sebastianruziczka.de>
2019-02-11 22:12:37 +0100

    switched tests

--- ci/execExampleTests.sh
+++ ci/execExampleTests.sh
@@ -1,52 +1,6 @@
 set -e
 
-echo ">>>>>>> creating test-repository"
-mkdir endToEndTest
-cd endToEndTest
 git clone https://github.com/RosesTheN00b/gradle-cobol-plugin-example
-git clone https://github.com/RosesTheN00b/gradle-cobol-plugin
+cd gradle-cobol-plugin-example
+sh ci/run_unit_dev_test.sh
 cd ..
-echo "<<<<<<<"
-
-echo ">>>>>>> create local repo for this jar"
-gradle publish -PgenerateLatest
-mv ../repo endToEndTest/repo
-echo "<<<<<<<"
-
-echo ">>>>>>> prepare test-repositories"
-gradle generateVersions switchOffline
-echo "<<<<<<<"
-
-echo ">>>>>>> create local repo for gradle plugin jar"
-cd endToEndTest/gradle-cobol-plugin
-gradle publish -PgenerateLatest
-cd ..
-cd ..
-echo "<<<<<<<"
-
-echo ">>>>>>> install gnu cobol"
-cd endToEndTest/gradle-cobol-plugin-example/
-sh ci/install_requirements.sh
-cd ..
-cd ..
-echo "<<<<<<<"
-
-
-echo ">>>>>>> exec test"
-cd endToEndTest/gradle-cobol-plugin-example/project-cobol-unit-test
-gradle check
-gradle testUnit computeTestCoverage  --refresh-dependencies --debug 
-cat build/CobolUnit/coverage.xml
-cd ..
-cd ..
-cd ..
-echo "<<<<<<<"
-
-
-echo ">>>>>>> exec test"
-cd endToEndTest/gradle-cobol-plugin-example/project-cobol-integration-test
-gradle check
-gradle testIntegration computeIntegrationTestCoverage  --refresh-dependencies --debug
-cat build/CobolUnit/integration-coverage.xml
-echo "<<<<<<<"
-
